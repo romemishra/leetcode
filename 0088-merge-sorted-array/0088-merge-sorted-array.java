54c0e1ebf5074c52a2864a7332ed7459f5b96888
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-09 20:18:56 +0530

    Time: 0 ms (100%), Space: 43.8 MB (50.63%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,8 +1,8 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1; // last element in nums1
-        int j = n - 1; // last element in nums2
-        int k = m + n - 1; // last position in nums1
+        int i = m - 1; 
+        int j = n - 1; 
+        int k = m + n - 1; 
 
         // Merge from back
         while (i >= 0 && j >= 0) {
