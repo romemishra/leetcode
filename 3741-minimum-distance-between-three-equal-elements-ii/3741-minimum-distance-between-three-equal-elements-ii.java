c15fa72539f92ede0a4fa3cc11afa3e2d0fd3c8a
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-11 23:36:17 +0530

    Time: 6 ms (100%), Space: 161.1 MB (99.44%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
@@ -1,26 +1,21 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            ArrayList<Integer> l=map.getOrDefault(nums[i],new ArrayList<>());
-            l.add(i);
-            map.put(nums[i],l);
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        int prev1[] = new int[n+1];
+        int prev2[] = new int[n+1];
+        for(int i=0;i<n+1;i++){
+            prev1[i] = prev2[i] = -1;
         }
-        int min=Integer.MAX_VALUE;
-        for(int key:map.keySet()){
-            if(map.get(key).size()<3) continue;
-            ArrayList<Integer> temp=map.get(key);
-            int i=0;
-            int j=1;
-            int k=2;
-            while(k<temp.size()){
-                min=Math.min(min,2*(Math.max(temp.get(i),Math.max(temp.get(j),temp.get(k)))-Math.min(temp.get(i),Math.min(temp.get(j),temp.get(k)))));
-                i++;
-                j++;
-                k++;
+        
+        for(int i=0;i<n;i++){
+            int value = nums[i];
+            if(prev2[value] != -1){
+                ans = Math.min(ans, (i-prev2[value]));
             }
+            prev2[value] = prev1[value];
+            prev1[value] = i;
         }
-        return min==Integer.MAX_VALUE?-1:min;
-
+        return (ans==Integer.MAX_VALUE)? -1: 2*ans;
     }
 }
