059e5220c882d0edebfafad6f4606e1260ff506f
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-11 23:38:29 +0530

    Time: 6 ms (100%), Space: 161.2 MB (99.44%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.java
@@ -1,23 +1,21 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        Map<Integer, int[]> map = new HashMap<>();
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            int x = nums[i];
-            if (!map.containsKey(x)) {
-                map.put(x, new int[] {-1, i});
-            } else {
-                var arr = map.get(x);
-                if (arr[0] != -1) {
-                    min = Math.min(2 * (i - arr[0]), min);
-                }
-                arr[0] = arr[1];
-                arr[1] = i;
+        int n = nums.length;
+        int ans = Integer.MAX_VALUE;
+        int prev1[] = new int[n+1];
+        int prev2[] = new int[n+1];
+        for(int i=0;i<n+1;i++){
+            prev1[i] = prev2[i] = -1;
+        }
+        
+        for(int i=0;i<n;i++){
+            int value = nums[i];
+            if(prev2[value] != -1){
+                ans = Math.min(ans, (i-prev2[value]));
             }
+            prev2[value] = prev1[value];
+            prev1[value] = i;
         }
-        if (min == Integer.MAX_VALUE) {
-            min = -1;
-        }
-        return min;
+        return (ans==Integer.MAX_VALUE)? -1: 2*ans;
     }
 }
