22e6343ccb0039983d9bfdd937e67e8f6fba469a
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-13 00:08:57 +0530

    Time: 4 ms (100%), Space: 42.3 MB (100%) - LeetHub

--- 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.java
+++ 1320-minimum-distance-to-type-a-word-using-two-fingers/1320-minimum-distance-to-type-a-word-using-two-fingers.java
@@ -1,35 +1,31 @@
 class Solution {
-    Integer[][][] dp;
-
     public int minimumDistance(String word) {
         int n = word.length();
-        dp = new Integer[n][27][27]; // 26 letters + 1 for "no finger"
-        return solve(word, 0, -1, -1);
+        if(n < 3) return 0;
+        
+        char[] arr = word.toCharArray();
+        int[] store = new int[27];
+        for(int i=0; i<27; ++i) store[i] = Integer.MAX_VALUE;
+        
+        store[26] = dist(arr[1], arr[0]);
+        store[arr[0]-'A'] = 0;
+        for(int k=2; k<n; ++k) {
+            int delta = dist(arr[k], arr[k-1]), min = store[26];
+            for(int i=0; i<27; ++i) {
+                if(store[i] < min) min = Math.min(min, store[i] + dist(arr[k], (char) (i + 'A')));
+                if(store[i] < Integer.MAX_VALUE) store[i] += delta;
+            }
+            store[arr[k-1]-'A'] = Math.min(store[arr[k-1]-'A'], min);
+        }
+        
+        int ans = Integer.MAX_VALUE;
+        for(int x: store) ans = Math.min(ans, x);
+        
+        return ans;
     }
-
-    private int solve(String word, int i, int f1, int f2) {
-        if (i == word.length()) return 0;
-
-        if (dp[i][f1 + 1][f2 + 1] != null)
-            return dp[i][f1 + 1][f2 + 1];
-
-        int curr = word.charAt(i) - 'A';
-
-        // Option 1: use finger1
-        int cost1 = dist(f1, curr) + solve(word, i + 1, curr, f2);
-
-        // Option 2: use finger2
-        int cost2 = dist(f2, curr) + solve(word, i + 1, f1, curr);
-
-        return dp[i][f1 + 1][f2 + 1] = Math.min(cost1, cost2);
+    
+    private int dist(char a, char b) {
+        int i = a - 'A', j = b - 'A';
+        return Math.abs(i/6 - j/6) + Math.abs(i%6 - j%6);
     }
-
-    private int dist(int a, int b) {
-        if (a == -1) return 0; // initial free position
-
-        int x1 = a / 6, y1 = a % 6;
-        int x2 = b / 6, y2 = b % 6;
-
-        return Math.abs(x1 - x2) + Math.abs(y1 - y2);
-    }
-}
+}        
