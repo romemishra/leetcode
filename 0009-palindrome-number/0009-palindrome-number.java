3701eff94d0241e7b63501cf336b7e32b6b2b9db
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-12 23:54:10 +0530

    Time: 5 ms (79.59%), Space: 46.2 MB (23.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isPalindrome(int x) {
-         if (x < 0) return false;
 
-        int original = x;
-        int reversed = 0;
-
-        while (x != 0) {
-            int digit = x % 10;
-            reversed = reversed * 10 + digit;
-            x = x / 10;
+        if (x < 0 || (x % 10 == 0 && x != 0)){
+            return false;
         }
 
-        return original == reversed;
+        int reversedHalf = 0;
+        while(x > reversedHalf) {
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x = x /10;
+        }
+        return (x == reversedHalf || x == reversedHalf / 10);
+
+        
     }
 }
