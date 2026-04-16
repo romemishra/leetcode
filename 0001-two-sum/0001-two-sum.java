e9dd31744fec92d0c88cd05eba1a23eff2bed4bd
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-16 15:30:46 +0530

    Time: 66 ms (6.29%), Space: 46.9 MB (76%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numToIndex = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (numToIndex.containsKey(complement)) {
-                return new int[] {numToIndex.get(complement), i};
-            }
-            numToIndex.put(nums[i], i);
+        int size=nums.length;
+        int[] result =new int[2];
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++)
+            if(nums[i]+nums[j]==target)
+            result=new int[]{i,j};
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return result;
+        
     }
 }
