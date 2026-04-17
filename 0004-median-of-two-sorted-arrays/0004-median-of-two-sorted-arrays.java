05879255992726475f1241ffbd4b0d877dfe23f7
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-17 11:19:28 +0530

    Time: 5 ms (26%), Space: 49 MB (19.58%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,41 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length > nums2.length) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-
-        int m = nums1.length, n = nums2.length;
-        int low = 0, high = m;
-
-        while (low <= high) {
-            int cut1 = (low + high) / 2;
-            int cut2 = (m + n + 1 ) / 2 - cut1;
-
-            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-
-            int r1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
-            int r2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
-
-            if (l1 <= r2 && l2 <= r1) {
-                if ((m + n) % 2 == 0) {
-                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
-                } else {
-                    return Math.max(l1, l2);
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        
+        int m = nums1.length;
+        int n = nums2.length;
+        
+        int i=0;
+        int j=0;
+        
+        while(pq.size()<=(m+n)/2){
+            
+            
+            if(i<m && j<n){
+                
+                if(nums1[i]<nums2[j]){
+                    pq.add(nums1[i]);
+                    i++;
+                }else{
+                    pq.add(nums2[j]);
+                    j++;
                 }
-            } else if (l1 > r2) {
-                high = cut1 - 1;
-            } else {
-                low = cut1 + 1;
+                
+            }else if(i>=m){
+                 pq.add(nums2[j]);
+                 j++;
+            }else if(j>=n){
+                 pq.add(nums1[i]);
+                 i++;
             }
+            
         }
-
-        return 0.0;
+        
+        if((m+n)%2==0){
+            return ((double)((pq.poll()+pq.peek())))/2D;
+        }
+        
+        return pq.peek();
     }
 }
