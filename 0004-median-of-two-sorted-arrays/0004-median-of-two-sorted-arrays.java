0a085fec8c55b2a1863dbe26dad099a16812861c
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-17 11:21:22 +0530

    Time: 1 ms (100%), Space: 48.7 MB (88.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,57 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-      //using binary search 
-      int total = nums1.length + nums2.length;
-     
-      double median = 0.0;
-      // using bs for smaller array
-      if(nums1.length > nums2.length){
-           return findMedianSortedArrays(nums2 , nums1);
-       }
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
 
-       int l = 0 ;
-       int h = nums1.length;
+        int m = nums1.length, n = nums2.length;
+        int low = 0, high = m;
 
-      
-       while(l<= h){
+        while (low <= high) {
+            int cut1 = (low + high) / 2;
+            int cut2 = (m + n + 1) / 2 - cut1;
 
-        // partition points;
-       int cut1 =(l+h)/2;
-       int cut2= (total+1)/2 -cut1 ;
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
 
+            int r1 = (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
 
-        // leftm most in each partition
-        int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-        int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-
-        //right least
-       int r1 = (cut1 == nums1.length) ? Integer.MAX_VALUE : nums1[cut1];
-       int r2 = (cut2 == nums2.length) ? Integer.MAX_VALUE : nums2[cut2];
-
-        // comparisions
-        if(l1<= r2 && l2 <= r1){
-            // if even length
-            if(total % 2 == 0){
-                return median = (Math.max(l1 , l2) + Math.min(r1 , r2))/2.0; 
+            if (l1 <= r2 && l2 <= r1) {
+                if ((m + n) % 2 == 0) {
+                    return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                } else {
+                    return Math.max(l1, l2);
+                }
+            } else if (l1 > r2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
             }
-            else{
-                return median = Math.max (l1 , l2);
-            }
-        
-        }
-        // search in left 
-        else if(l1> r2){
-            h = cut1 - 1;
-        }
-        else{
-            l = cut1+1;
         }
 
-      
-       }
-       
-        return 0.0 ;
-       
-
+        return 0.0;
     }
 }
