9c9ae859edcc0f0c82c35eae8d98c8b5f00e0acb
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-17 11:20:03 +0530

    Time: 2 ms (46.3%), Space: 48.6 MB (88.43%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,41 +1,57 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        
-        int m = nums1.length;
-        int n = nums2.length;
-        
-        int i=0;
-        int j=0;
-        
-        while(pq.size()<=(m+n)/2){
-            
-            
-            if(i<m && j<n){
-                
-                if(nums1[i]<nums2[j]){
-                    pq.add(nums1[i]);
-                    i++;
-                }else{
-                    pq.add(nums2[j]);
-                    j++;
-                }
-                
-            }else if(i>=m){
-                 pq.add(nums2[j]);
-                 j++;
-            }else if(j>=n){
-                 pq.add(nums1[i]);
-                 i++;
+      //using binary search 
+      int total = nums1.length + nums2.length;
+     
+      double median = 0.0;
+      // using bs for smaller array
+      if(nums1.length > nums2.length){
+           return findMedianSortedArrays(nums2 , nums1);
+       }
+
+       int l = 0 ;
+       int h = nums1.length;
+
+      
+       while(l<= h){
+
+        // partition points;
+       int cut1 =(l+h)/2;
+       int cut2= (total+1)/2 -cut1 ;
+
+
+        // leftm most in each partition
+        int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+        int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+        //right least
+       int r1 = (cut1 == nums1.length) ? Integer.MAX_VALUE : nums1[cut1];
+       int r2 = (cut2 == nums2.length) ? Integer.MAX_VALUE : nums2[cut2];
+
+        // comparisions
+        if(l1<= r2 && l2 <= r1){
+            // if even length
+            if(total % 2 == 0){
+                return median = (Math.max(l1 , l2) + Math.min(r1 , r2))/2.0; 
             }
-            
-        }
+            else{
+                return median = Math.max (l1 , l2);
+            }
         
-        if((m+n)%2==0){
-            return ((double)((pq.poll()+pq.peek())))/2D;
         }
-        
-        return pq.peek();
+        // search in left 
+        else if(l1> r2){
+            h = cut1 - 1;
+        }
+        else{
+            l = cut1+1;
+        }
+
+      
+       }
+       
+        return 0.0 ;
+       
+
     }
 }
