9298b279ab27c14a72183ee4adab26061d46531d
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-18 14:32:31 +0530

    Time: 59 ms (30.65%), Space: 99.1 MB (30.15%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
@@ -1,26 +1,28 @@
-import java.util.*;
-
 class Solution {
-    public int minMirrorPairDistance(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int minDist = Integer.MAX_VALUE;
 
-        for (int j = 0; j < nums.length; j++) {
-            // Check if current number matches any stored reverse
-            Integer i = map.get(nums[j]);
-            if (i != null) {
-                minDist = Math.min(minDist, j - i);
+    public int minMirrorPairDistance(int[] nums) {
+        Map<Integer, Integer> lastIndexMap = new HashMap<>();
+        int best = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer lastIndex = lastIndexMap.get(nums[i]);
+
+            if (lastIndex != null) {
+                best = Math.min(best, i - lastIndex);
+
+                if (best == 1) {
+                    return 1;
+                }
+
             }
 
-            // Store reverse of current number
-            int rev = reverse(nums[j]);
-            map.put(rev, j);
+            lastIndexMap.put(reverseInt(nums[i]), i);
         }
 
-        return (minDist == Integer.MAX_VALUE) ? -1 : minDist;
+        return best == Integer.MAX_VALUE ? -1 : best;
     }
 
-    private int reverse(int x) {
+    private int reverseInt(int x) {
         int rev = 0;
         while (x > 0) {
             rev = rev * 10 + (x % 10);
@@ -28,4 +30,13 @@ class Solution {
         }
         return rev;
     }
+
+    /*
+    private int reverseInt(int x) {
+        String xStr = "" + x;
+        String xReversed = new StringBuilder(xStr).reverse().toString();
+        return Integer.parseInt(xReversed);
+    }
+    */
+
 }
