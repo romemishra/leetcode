b4dd3eb08f554d12ceb2699512027ad4e9ffb305
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-18 14:33:05 +0530

    Time: 46 ms (97.99%), Space: 94.8 MB (97.99%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
@@ -1,42 +1,21 @@
 class Solution {
-
     public int minMirrorPairDistance(int[] nums) {
-        Map<Integer, Integer> lastIndexMap = new HashMap<>();
-        int best = Integer.MAX_VALUE;
-
-        for (int i = 0; i < nums.length; i++) {
-            Integer lastIndex = lastIndexMap.get(nums[i]);
-
-            if (lastIndex != null) {
-                best = Math.min(best, i - lastIndex);
-
-                if (best == 1) {
-                    return 1;
-                }
-
+        int n = nums.length;
+        int ans = n;
+        Map<Integer, Integer> lastIdx = new HashMap<>();
+        for (int j = 0; j < n; j++) {
+            int x = nums[j];
+            Integer i = lastIdx.get(x);
+            if (i != null) {
+                ans = Math.min(ans, j - i);
             }
 
-            lastIndexMap.put(reverseInt(nums[i]), i);
+            int rev = 0;
+            for (; x > 0; x /= 10) {
+                rev = rev * 10 + x % 10;
+            }
+            lastIdx.put(rev, j);
         }
-
-        return best == Integer.MAX_VALUE ? -1 : best;
+        return ans < n ? ans : -1;
     }
-
-    private int reverseInt(int x) {
-        int rev = 0;
-        while (x > 0) {
-            rev = rev * 10 + (x % 10);
-            x /= 10;
-        }
-        return rev;
-    }
-
-    /*
-    private int reverseInt(int x) {
-        String xStr = "" + x;
-        String xReversed = new StringBuilder(xStr).reverse().toString();
-        return Integer.parseInt(xReversed);
-    }
-    */
-
 }
