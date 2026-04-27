0e166b75ea33731ab605e04963987eeea631cf68
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-27 15:52:32 +0530

    Time: 13 ms (90.59%), Space: 93.1 MB (70.46%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
@@ -1,57 +1,63 @@
 class Solution {
+    
+    // incomingDir 0:left, 1: up, 2:right, 3: down
+    static int[][] DIRS = {{0,-1},{-1,0},{0,1},{1,0}};
+    int m, n;
+    int[][] grid;
+    boolean[][] visited;
+
+    static int[][] OUT = {
+        {0, 2}, 
+        {1, 3},
+        {0, 3},
+        {2, 3},
+        {0, 1},
+        {1, 2}
+    };
+
     public boolean hasValidPath(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        boolean[][] vis = new boolean[m][n];
-        
-        java.util.Queue<int[]> q = new java.util.LinkedList<>();
-        q.offer(new int[]{0, 0});
-        vis[0][0] = true;
-        
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int r = cur[0], c = cur[1];
-            
-            if (r == m - 1 && c == n - 1) return true;
-            
-            int type = grid[r][c];
-            
-            // UP
-            if ((type == 2 || type == 5 || type == 6) && r > 0) {
-                int next = grid[r - 1][c];
-                if (!vis[r - 1][c] && (next == 2 || next == 3 || next == 4)) {
-                    vis[r - 1][c] = true;
-                    q.offer(new int[]{r - 1, c});
-                }
-            }
-            
-            // DOWN
-            if ((type == 2 || type == 3 || type == 4) && r < m - 1) {
-                int next = grid[r + 1][c];
-                if (!vis[r + 1][c] && (next == 2 || next == 5 || next == 6)) {
-                    vis[r + 1][c] = true;
-                    q.offer(new int[]{r + 1, c});
-                }
-            }
-            
-            // LEFT
-            if ((type == 1 || type == 3 || type == 5) && c > 0) {
-                int next = grid[r][c - 1];
-                if (!vis[r][c - 1] && (next == 1 || next == 4 || next == 6)) {
-                    vis[r][c - 1] = true;
-                    q.offer(new int[]{r, c - 1});
-                }
-            }
-            
-            // RIGHT
-            if ((type == 1 || type == 4 || type == 6) && c < n - 1) {
-                int next = grid[r][c + 1];
-                if (!vis[r][c + 1] && (next == 1 || next == 3 || next == 5)) {
-                    vis[r][c + 1] = true;
-                    q.offer(new int[]{r, c + 1});
+        this.grid = grid;
+        m = grid.length;
+        n = grid[0].length;
+        visited = new boolean[m][n];
+
+        int incomingDir = 0;
+        return dfs(0, 0, incomingDir);
+    }
+    
+
+    boolean dfs(int r, int c, int incomingDir) {
+        visited[r][c] = true;
+        if (r == m - 1 && c == n - 1) return true;
+
+        boolean canReach = false;
+
+        int street = grid[r][c];
+
+        for (int dirIdx : OUT[street - 1]) {
+            int[] dir = DIRS[dirIdx];
+            int nr = r + dir[0], nc = c + dir[1];
+            if (
+                nr >= 0 && nr < m && nc >= 0 && nc < n
+                && !visited[nr][nc] && accept(grid[nr][nc], dirIdx)
+            ) {
+                if (dfs(nr, nc, dirIdx)) {
+                    canReach = true;
                 }
             }
         }
-        
+
+        return canReach;
+    }
+
+
+    boolean accept(int street, int incomingDir) {
+        if (street == 1) return incomingDir == 0 || incomingDir == 2;
+        if (street == 2) return incomingDir == 1 || incomingDir == 3;
+        if (street == 3) return incomingDir == 2 || incomingDir == 1;
+        if (street == 4) return incomingDir == 0 || incomingDir == 1;
+        if (street == 5) return incomingDir == 2 || incomingDir == 3;
+        if (street == 6) return incomingDir == 0 || incomingDir == 3;
         return false;
     }
 }
