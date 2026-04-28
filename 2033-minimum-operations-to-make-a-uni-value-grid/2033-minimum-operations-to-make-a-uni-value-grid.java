52437003d9f9ef52ecf28cddbee797ca1426711b
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-04-28 22:08:16 +0530

    Time: 8 ms (100%), Space: 107.7 MB (81.68%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,39 +1,32 @@
 class Solution {
     public int minOperations(int[][] grid, int x) {
-        List<Integer> list = new ArrayList<>();
-        
-        // Flatten grid
+        int n = grid.length, m = grid[0].length;
+        int[] freq = new int[10001];
+        //check -1 cases
+        boolean foundFalse = false;
+        int currChoice = grid[0][0];
         for (int[] row : grid) {
-            for (int num : row) {
-                list.add(num);
+            for (int y : row) {
+                freq[y]++;
+                if (Math.abs(currChoice - y) % x != 0)
+                    foundFalse = true;
             }
         }
-        
-        // Check feasibility
-        int base = list.get(0);
-        for (int num : list) {
-            if ((num - base) % x != 0) {
-                return -1;
+        if (foundFalse) return -1;
+        //sorting
+        int[] arr = new int[n * m];
+        int k = 0;
+        for (int i = 0; i < 10001; i++) {
+            for (int j = 0; j < freq[i]; j++) {
+                arr[k++] = i;
             }
         }
-        
-        // Normalize
-        for (int i = 0; i < list.size(); i++) {
-            list.set(i, list.get(i) / x);
+        int count = 0;
+        //pick best middle element
+        int median = arr[arr.length / 2];
+        for (int num : arr) {
+            count += Math.abs(num - median) / x;
         }
-        
-        // Sort
-        Collections.sort(list);
-        
-        // Median
-        int median = list.get(list.size() / 2);
-        
-        // Count operations
-        int operations = 0;
-        for (int num : list) {
-            operations += Math.abs(num - median);
-        }
-        
-        return operations;
+        return count;
     }
 }
