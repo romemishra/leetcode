d1709e8d0a8c915c3b22ac1875fd7e23df821dab
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-05-06 23:15:15 +0530

    Time: 4 ms (100%), Space: 124.9 MB (43.27%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.java
+++ 1861-rotating-the-box/1861-rotating-the-box.java
@@ -1,39 +1,75 @@
 class Solution {
     public char[][] rotateTheBox(char[][] boxGrid) {
-        int m = boxGrid.length;
-        int n = boxGrid[0].length;
+        
+        // n x m rotated grid
+        char[][] rotatedGrid = new char[boxGrid[0].length][boxGrid.length];
 
-        // Step 1: simulate gravity to the right
-        for (int i =0 ; i < m; i++) {
-
-            int write = n - 1;
-
-            for (int j = n - 1; j >= 0; j--) {
-
-                if (boxGrid[i][j] == '*') {
-                    write = j - 1;
-                }
-
-                else if (boxGrid[i][j] == '#') {
-
-                    boxGrid[i][j] = '.';
-                    boxGrid[i][write] = '#';
-
-                    write--;
-                }
-            }
+        for (int i = 0; i < boxGrid.length; i++) {
+            processRow(boxGrid[i], rotatedGrid, boxGrid.length - 1 - i);
         }
 
-        // Step 2: rotate matrix clockwise
-        char[][] ans = new char[n][m];
+        return rotatedGrid;
+    }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
 
-                ans[j][m - 1 - i] = boxGrid[i][j];
+    private void processRow(char[] row, char[][] rotatedGrid, int x) {
+
+        int lowestBarrier = row.length;
+        for (int curr = row.length - 1; curr >= 0; curr--) {
+            
+            if (row[curr] == '#') {
+                lowestBarrier -= 1;
+                rotatedGrid[lowestBarrier][x] = '#';
+
+                if (curr != lowestBarrier) rotatedGrid[curr][x] = '.';
+            } else if (row[curr] == '*') {
+                lowestBarrier = curr;
+                rotatedGrid[lowestBarrier][x] = '*';
+            } else {
+                rotatedGrid[curr][x] = '.';
             }
-        }
 
-        return ans;
+        }
     }
 }
+
+//     public char[][] rotateTheBox(char[][] boxGrid) {
+        
+//         // n x m rotated grid
+//         char[][] rotatedGrid = new char[boxGrid[0].length][boxGrid.length];
+
+//         for (int i = 0; i < boxGrid.length; i++) {
+//             char[] newColumn = processRow(boxGrid[i]);
+
+//             for (int j = 0; j < newColumn.length; j++) {
+
+//                 rotatedGrid[j][boxGrid.length - 1 - i] = newColumn[j];
+//             }
+//         }
+
+//         return rotatedGrid;
+//     }
+
+
+//     private char[] processRow(char[] row) {
+
+//         char[] newColumn = new char[row.length];
+
+//         int lowestBarrier = row.length;
+//         for (int curr = row.length - 1; curr >= 0; curr--) {
+            
+//             if (row[curr] == '#') {
+//                 lowestBarrier -= 1;
+//                 newColumn[lowestBarrier] = '#';
+//                 if (curr != lowestBarrier) newColumn[curr] = '.';
+//             } else if (row[curr] == '*') {
+//                 lowestBarrier = curr;
+//                 newColumn[lowestBarrier] = '*';
+//             } else {
+//                 newColumn[curr] = '.';
+//             }
+
+//         }
+
+//         return newColumn;
+//     }
