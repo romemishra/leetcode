4d87d97ad591d0a71045b7ddd6ef7f456d1406c2
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-05-07 21:20:16 +0530

    Time: 606 ms (6.45%), Space: 199.1 MB (43.55%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.java
+++ 3660-jump-game-ix/3660-jump-game-ix.java
@@ -1,46 +1,27 @@
 class Solution {
     public int[] maxValue(int[] nums) {
         int n = nums.length;
+        if (n == 0) 
+            return new int[0];
 
-        int[] prefixMax = new int[n];
-        int[] suffixMin = new int[n];
+        int[] pre = new int[n];
+        int[] suff = new int[n];
+        int[] res = new int[n];
 
-        // Build prefix maximum
-        prefixMax[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
-        }
+        pre[0] = nums[0];
+        for (int i = 1; i < n; i++) 
+            pre[i] = Math.max(nums[i], pre[i - 1]);
 
-        // Build suffix minimum
-        suffixMin[n - 1] = nums[n - 1];
+        suff[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) 
+            suff[i] = Math.min(nums[i], suff[i + 1]);
+
+        res[n - 1] = pre[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            suffixMin[i] = Math.min(suffixMin[i + 1], nums[i]);
+            res[i] = pre[i];
+            if (pre[i] > suff[i + 1]) 
+                res[i] = res[i + 1];
         }
-
-        int[] ans = new int[n];
-
-        int start = 0;
-        int currentMax = nums[0];
-
-        for (int i = 0; i < n; i++) {
-
-            currentMax = Math.max(currentMax, nums[i]);
-
-            // End of connected component
-            if (i == n - 1 || prefixMax[i] <= suffixMin[i + 1]) {
-
-                for (int j = start; j <= i; j++) {
-                    ans[j] = currentMax;
-                }
-
-                start = i + 1;
-
-                if (start < n) {
-                    currentMax = nums[start];
-                }
-            }
-        }
-
-        return ans;
+        return res;
     }
 }
