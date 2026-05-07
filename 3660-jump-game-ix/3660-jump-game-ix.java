b9fce5e14dc362920e69f986845af2f23add46b6
ANUGRAH MISHRA <anugrahm573@gmail.com>
2026-05-07 21:21:14 +0530

    Time: 6 ms (80.65%), Space: 199.3 MB (29.03%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.java
+++ 3660-jump-game-ix/3660-jump-game-ix.java
@@ -1,14 +1,27 @@
 class Solution {
     public int[] maxValue(int[] nums) {
-        int n = nums.length, min = Integer.MAX_VALUE;
-        int[] max = new int[n], ans = new int[n];
-        max[0] = nums[0];
-        for(int i = 1; i < n; i++) max[i] = Math.max(max[i - 1], nums[i]);
+        int n = nums.length;
+        if (n == 0) 
+            return new int[0];
 
-        for(int i = n - 2; i >= 0; i--) {
-            min = Math.min(min, nums[i + 1]);
-            if(min < max[i]) max[i] = max[i + 1];
+        int[] pre = new int[n];
+        int[] suff = new int[n];
+        int[] res = new int[n];
+
+        pre[0] = nums[0];
+        for (int i = 1; i < n; i++) 
+            pre[i] = Math.max(nums[i], pre[i - 1]);
+
+        suff[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) 
+            suff[i] = Math.min(nums[i], suff[i + 1]);
+
+        res[n - 1] = pre[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            res[i] = pre[i];
+            if (pre[i] > suff[i + 1]) 
+                res[i] = res[i + 1];
         }
-        return max;
+        return res;
     }
 }
